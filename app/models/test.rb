c38f12738e681ccc7a74934c0ebd0dca2979c0b7
skrepkoed <faust898@mail.ru>
2021-03-01 00:17:52 +0500

    Реализован инстанс метод completed_tests модели User, который возвращает список пройденных тестов определенного уровня сложности. Реализован метод tests_by_category класса Test, который возвращает тесты по определенной теме. Добавлен гем pry

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.tests_by_category(title)
+    category = Category.where(title: title)
+    category.map { |category| Test.where(category_id: category.id).order('id DESC') }.flatten.to_a
+  end
 end
