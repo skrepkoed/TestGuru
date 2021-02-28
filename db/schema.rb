32f2b29b415148c2b5c41da4da64dbaa045a2811
skrepkoed <faust898@mail.ru>
2021-02-28 21:46:50 +0500

    Создана соединительная таблица тесты-пользователи

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_073610) do
+ActiveRecord::Schema.define(version: 2021_02_28_164114) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2021_02_28_073610) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.index ["test_id"], name: "index_tests_users_on_test_id"
+    t.index ["user_id"], name: "index_tests_users_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
