01507e8d089ad6fbe69c9d5a89a35148c6494967
skrepkoed <faust898@mail.ru>
2021-02-28 17:24:19 +0500

    Добавлен генератор вопросов, В каждый тест добавлено по 10 вопросов.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require_relative './seeds/001_categories'
 require_relative './seeds/002_tests'
+require_relative './seeds/003_questions'
 Category.create(CategorySeed.seed(5)) unless Category.count == 5
 Category.select(:id).each { |category| Test.create(TestSeed.seed(5, category.id)) }
+Test.select(:id).each { |test| Question.create(QuestionSeed.seed(10, test.id)) }
---
+++ db/seeds/003_questions.rb
@@ -0,0 +1,10 @@
+class QuestionSeed
+  def self.seed(number, test_id)
+    data = []
+    number.times do
+      data << { body: Faker::Lorem.question(word_count: 3, supplemental: true),
+                test_id: test_id }
+    end
+    data
+  end
+end
