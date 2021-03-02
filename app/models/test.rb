d0cbfb56afc76d47c8cfbf1a6699ab5f2f70aead
skrepkoed <faust898@mail.ru>
2021-03-02 22:46:06 +0500

    Настроены связи между моделями в соответствии с ER диаграммой

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  has_one :user, through: :authorships
   def self.tests_by_category(title)
     joins('JOIN categories ON tests.category_id=categories.id').where('categories.title=?', title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+  has_many :authorships
+  has_many :tests, through: :authorships
   def completed_tests(level)
-    Test.joins('JOIN tests_users ON tests.id=tests_users.test_id').where('level=? AND user_id=?', level, id).pluck(:title)
+    Test.joins('JOIN tests_users ON tests.id=tests_users.test_id').where('level=? AND user_id=?', level,
+                                                                         id).pluck(:title)
   end
 end
