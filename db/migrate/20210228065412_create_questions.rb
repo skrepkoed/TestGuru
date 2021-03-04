10c65b1d3099b5466645c48dcb2cfb53ea123b1e
skrepkoed <faust898@mail.ru>
2021-03-04 23:56:25 +0500

    Добавлена колонка email для таблицы users. Убрано ограничение null:false для вопросов и ответов

--- db/migrate/20210228065412_create_questions.rb
+++ db/migrate/20210228065412_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
       t.text :body, null: false
-      t.references :test, null: false, foreign_key: true
+      t.references :test, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20210228070227_create_answers.rb
+++ db/migrate/20210228070227_create_answers.rb
@@ -3,7 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
     create_table :answers do |t|
       t.text :body, null: false
       t.boolean :correct, null: false
-      t.references :question, null: false, foreign_key: true
+      t.references :question,  foreign_key: true
       t.timestamps
     end
   end
