21722ebde9a7c008d5675230177a3e3e18f872a7
skrepkoed <faust898@mail.ru>
2021-03-04 21:45:43 +0500

    Добавлены скоупы для выбора правильных Ответов Для выбора Тестов по уровню сложности scope-метод по умолчанию для получения Категорий, отсортированных по названию в порядке возрастания

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,9 @@ class Test < ApplicationRecord
   has_one :authorship
   has_many :passed_users, through: :tests_users, source: :user
   has_one :author, through: :authorship, source: :user
+  scope :easy, ->{where(level:0..1)}
+  scope :middle, ->{where(level:2..4)}
+  scope :hard, ->{where(level:5..Float::INFINITY)}
   def self.tests_by_category(title)
     joins(:category).where('categories.title=?',title)
   end
