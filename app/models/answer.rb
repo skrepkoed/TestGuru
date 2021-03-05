b676270e8c8cbed046e23313a8176c85dd53b323
skrepkoed <faust898@mail.ru>
2021-03-05 21:05:05 +0500

    Валидация количества ответов принадлежащих вопросу перенесена в модель Answer.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
   scope :correct_answers, ->{where(correct:true)}
   validates :body, presence:true
+  validate :validate_question_number_answers
+  
+  private
+
+  def validate_question_number_answers
+  	if self.class.where(question_id:question_id).count>=4
+  		errors.add(:question_id, message:'Question can contain 4 answers maximum')
+  	end 
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,11 +2,4 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
   validates :body, presence:true
-  validate :validate_question_number_answers
-  
-  private
-
-  def validate_question_number_answers
-  	errors.add(:answers) unless (1..4).include?(answers.count)
-  end
 end
