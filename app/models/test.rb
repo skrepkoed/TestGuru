3395fa28d3a8815ab8b2198f7332db3b037a3acf
skrepkoed <faust898@mail.ru>
2021-03-05 21:50:30 +0500

    Удалена модель Authorship. Теперь тест принадлежит одному автору, а автор имеет много тестов.

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,14 +2,16 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
   has_many :tests_users
-  has_one :authorship
   has_many :passed_users, through: :tests_users, source: :user
-  has_one :author, through: :authorship, source: :user
+  belongs_to :user
   scope :easy, ->{where(level:0..1)}
   scope :middle, ->{where(level:2..4)}
   scope :hard, ->{where(level:5..Float::INFINITY)}
   scope :tests_by_category, ->(title){joins(:category).where('categories.title=?',title)}
   validates :title, presence: true
   validates :level, numericality: {only_intger:true}
-  validates :title, uniqueness:{scope: :level, message:'Title and level combinztion must be unique'},on:[:create,:update]
+  validates :title, uniqueness:{scope: :level, message:'Title and level combinztion must be unique'}
+  def self.tests_by_category(title)
+    joins(:category).where('categories.title=?',title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users
   has_many :passed_tests, through: :tests_users, source: :test
-  has_many :authorships
-  has_many :test_authorships, through: :authorships, source: :test
+  has_many :tests
   validates :email, presence:true
   def completed_tests(level)
     tests.where(level:level)
