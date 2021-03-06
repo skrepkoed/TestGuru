e9a277a6bc0943405868bb2fb5ea9990bfb32569
skrepkoed <faust898@mail.ru>
2021-03-06 18:44:19 +0500

    Добавлен колбэк find_test, который находит тест, которому принадлежат вопросы, добавлен обработчик исключения, когда вопрос не найден. Добавлено вью для экшена show, из этого вью можно удалить вопрос.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,31 +1,40 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_record_not_found
   def index
     respond_to do |format|
-      format.html { render inline: '<%= Question.where(test_id:params[:test_id]).map { |question| question.body  }%>' }
+      format.html { render inline: '<%= @test.questions.map { |question| question.body  }%>' }
     end
   end
 
-  def new 
-  end
+  def new; end
 
   def create
-    Question.create(question_params)
-    redirect_to test_questions_path
+    @test.questions.create(question_params)
+    redirect_to test_questions_path(@test.id)
   end
 
   def show
-    question = Question.where(id: params[:id])
+    @question = Question.find(params[:id])
   end
 
   def destroy
+    test_id = Question.find(params[:id]).test.id
     Question.destroy(params[:id])
-    redirect_to test_questions_path
+    redirect_to test_questions_path(test_id)
   end
 
   private
 
   def question_params
-    params[:question][:test_id] = params[:test_id]
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def rescue_from_record_not_found
+    render inline: "<h1>Question with id:#{params[:id]} was not found</h1>"
   end
 end
