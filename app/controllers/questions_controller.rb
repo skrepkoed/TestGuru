5116441fa38f9a5693e55045b5ce6e46f0428f5c
skrepkoed <faust898@mail.ru>
2021-03-06 17:44:02 +0500

    Добавлен метод destroy для удаления вопросов. Метод index выводит возвращает html

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,27 +1,31 @@
 class QuestionsController < ApplicationController
   def index
-  	questions=Question.where(id:params[:test_id])
+    respond_to do |format|
+      format.html { render inline: '<%= Question.where(test_id:params[:test_id]).map { |question| question.body  }%>' }
+    end
   end
 
-  def new
+  def new 
   end
 
   def create
-  	Question.create(question_params)
-  	redirect_to test_questions_path
+    Question.create(question_params)
+    redirect_to test_questions_path
   end
 
   def show
-  	question=Question.where(id:params[:id])
+    question = Question.where(id: params[:id])
   end
 
   def destroy
+    Question.destroy(params[:id])
+    redirect_to test_questions_path
   end
 
   private
 
   def question_params
-  	params[:question][:test_id]=params[:test_id]
-  	params.require(:question).permit(:body,:test_id)
+    params[:question][:test_id] = params[:test_id]
+    params.require(:question).permit(:body, :test_id)
   end
 end
