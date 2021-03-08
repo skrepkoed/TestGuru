798017abbae6416a6280bd8bddfaaabb4662fafd
skrepkoed <faust898@mail.ru>
2021-03-08 18:08:57 +0500

    Реализован экшн show и его вью

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,15 @@
 class TestsController < ApplicationController
+	before_action :find_test, only: %i[new show edit]
 	def index
 		@tests=Test.all
+	end
+
+	def show
+	end
+
+	private
+
+	def find_test
+		@test=Test.find(params[:id])
 	end
 end
