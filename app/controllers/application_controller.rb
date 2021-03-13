458d9cd34b4f616133d5a9d5b133798f2cdd8278
skrepkoed <faust898@mail.ru>
2021-03-13 22:54:06 +0500

    К модели User добавлен метод admin?, реализовано перенаправление администраторов на страницу /admin/tests после логина

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   
+  private
+
+  def after_sign_in_path_for(resource)
+    store_location_for(:admin, admin_tests_path) if resource.admin?
+    stored_location_for(:admin) || super 
+  end
+
 end
