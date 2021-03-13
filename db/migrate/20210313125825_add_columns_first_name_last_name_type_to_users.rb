739e5eca2c01d520ba034db17c216d1a2df2d61d
skrepkoed <faust898@mail.ru>
2021-03-13 18:12:39 +0500

    Реализовано разделение на Администраторов и Пользователей посредством STI

---
+++ db/migrate/20210313125825_add_columns_first_name_last_name_type_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnsFirstNameLastNameTypeToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :type, :string, null: false, default: 'User'
+    add_index :users, :type
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_101847) do
+ActiveRecord::Schema.define(version: 2021_03_13_125825) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -76,9 +76,13 @@ ActiveRecord::Schema.define(version: 2021_03_13_101847) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "type", default: "User", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["type"], name: "index_users_on_type"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
