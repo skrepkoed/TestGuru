b1947f7a97d9c0e6656412381d2dd0ef24741bd3
skrepkoed <faust898@mail.ru>
2021-03-18 14:44:36 +0500

    В класс GitHubClient добавлен метод setup_github_client, получающий информацию о пользователе гитхаб

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,2 +1,12 @@
 class GitHubClient
+  attr_accessor :client
+  def initialize
+    @client=setup_github_client
+  end
+
+  private
+
+  def setup_github_client
+    Octokit::Client.new(access_token:ENV['PERSONAL_GITHUB_ACCESS_TOKEN'])
+  end
 end
