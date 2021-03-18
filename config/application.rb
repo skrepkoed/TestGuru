77a120a7c83e786f89e68f429879ca3f6dbf1a4a
skrepkoed <faust898@mail.ru>
2021-03-18 14:36:40 +0500

    Путь lib/client добавлен в путь автозагрузки констант.

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module TestGuru
     config.time_zone ="Ekaterinburg"
     config.i18n.available_locales = [:en, :ru]
 	config.i18n.default_locale = :ru
+    config.autoload_paths << "#{Rails.root}/lib/clients"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
---
+++ lib/clients/git_hub_client.rb
@@ -0,0 +1,2 @@
+class GitHubClient
+end
