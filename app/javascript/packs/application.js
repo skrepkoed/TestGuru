bd6e2987ed23fdc9747cde56d5aeb5180638c35e
skrepkoed <faust898@mail.ru>
2021-03-20 13:11:15 +0500

    Реализована функция сортировки тестов по названию с помощью sorting.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("utilities/sorting")
 import "bootstrap"
 import "../stylesheets/application"
 
